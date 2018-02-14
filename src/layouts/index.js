e441d92be1697be9f629cf1be63fcda5de7045c3
Justin Ball <justinball@gmail.com>
2018-02-13 23:16:51 -0700

    Remove debug

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,19 +13,14 @@ class Template extends React.Component {
     super(props)
     this.state = {
       isMenuVisible: false,
-      loading: 'is-loading',
+      //loading: 'is-loading',
+      loading: '',
     }
     this.handleToggleMenu = this.handleToggleMenu.bind(this)
-    console.log('constructing index and setting is-loading')
-    debugger;
   }
 
   componentDidMount() {
-    debugger;
-    console.log('index did mount')
     this.timeoutId = setTimeout(() => {
-      debugger;
-      console.log('changing loading state')
       this.setState({ loading: '' })
     }, 100)
   }
