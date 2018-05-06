e3d1d2ce37e5da3d3d7f8a498e523433b6c8d1f8
Justin Ball <justinball@gmail.com>
2018-05-06 16:12:18 -0600

    Started adding tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,10 @@
+const _ =  require('lodash');
 const path = require('path');
 const createPaginatedPages = require("gatsby-paginate");
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
+  const tagTemplate = path.resolve('src/templates/tags.js');
 
   return graphql(`
     {
@@ -19,6 +21,7 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
               title
               imageUrl
               structuredHtml
+              tags
               image {
                 childImageSharp {
                   sizes {
@@ -32,25 +35,51 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       }
     }
   `).then(result => {
+
     if (result.errors) {
       result.errors.forEach(e => console.error(e.toString()));
       return Promise.reject(result.errors);
     }
+
+    const edges = result.data.allMarkdownRemark.edges;
+
     createPaginatedPages({
-      edges: result.data.allMarkdownRemark.edges,
+      edges: edges,
       createPage: createPage,
       pageTemplate: "src/templates/archive.js",
       pageLength: 20,
       pathPrefix: "archive",
     });
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+
+    edges.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.path,
         component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
+      });
+    });
+
+    // Tag pages:
+    let tags = [];
+    // Iterate through each post, putting all found tags into `tags`
+    _.each(edges, edge => {
+      if (_.get(edge, "node.frontmatter.tags")) {
+        tags = tags.concat(edge.node.frontmatter.tags);
+      }
+    });
+
+    // Eliminate duplicate tags
+    tags = _.uniq(tags);
+
+    // Make tag pages
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag)}/`,
+        component: tagTemplate,
         context: {
-          path: node.frontmatter.path,
+          tag,
         },
       });
     });
+
   });
 };
